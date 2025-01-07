ed4424287adc450bd0f0e10084b3ce611c1cbe20
Ammar Sayed <ammarsayed669@gmail.com>
2025-01-07 17:21:30 +0200

    Time: 561 ms (21.42%), Space: 45 MB (6.7%) - LeetHub

--- 0816-design-hashset/0816-design-hashset.py
+++ 0816-design-hashset/0816-design-hashset.py
@@ -1,19 +1,39 @@
 class MyHashSet:
+    # brute force solution
+    # def __init__(self):
+    #     self.keys = list()
+    #     # values = []
 
+    # def add(self, key: int) -> None:
+    #     if key not in self.keys:
+    #         self.keys.append(key)
+
+    # def remove(self, key: int) -> None:
+    #     if key in self.keys:
+    #         self.keys.remove(key)
+
+    # def contains(self, key: int) -> bool:
+    #     if key in self.keys:
+    #         return True
+    #     return False
+
+    # solution 2
     def __init__(self):
-        self.keys = list()
+        self.Len = pow(10,6)
+        self.keys = [None for i in range(0, self.Len)]
         # values = []
 
     def add(self, key: int) -> None:
-        if key not in self.keys:
-            self.keys.append(key)
+        if key != self.keys[key%self.Len]:
+            self.keys[key%self.Len] = key
+
 
     def remove(self, key: int) -> None:
-        if key in self.keys:
-            self.keys.remove(key)
+        if key ==  self.keys[key%self.Len]:
+            self.keys[key%self.Len] = None
 
     def contains(self, key: int) -> bool:
-        if key in self.keys:
+        if key ==  self.keys[key%self.Len]:
             return True
         return False
 
