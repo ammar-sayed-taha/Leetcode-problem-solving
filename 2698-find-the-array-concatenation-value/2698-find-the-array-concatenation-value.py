a3170de4369fd4b156dce176520235c69803bcc1
Ammar Sayed <ammarsayed669@gmail.com>
2025-01-08 23:38:25 +0200

    Time: 0 ms (100%), Space: 17.9 MB (13.28%) - LeetHub

--- 2698-find-the-array-concatenation-value/2698-find-the-array-concatenation-value.py
+++ 2698-find-the-array-concatenation-value/2698-find-the-array-concatenation-value.py
@@ -4,6 +4,9 @@ class Solution:
         i = 0
         j = len(nums) - 1
         while i < j:
+            n = nums[j]
+            cnt = 0
+
             result +=  int(str(nums[i]) + str(nums[j]))
             i+=1
             j-=1
